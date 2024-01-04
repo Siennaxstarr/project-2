18e2bf3f4817040afcfcd7550a56b4cf4745b638
Kaila <kailaamia@gmail.com>
2024-01-03 22:17:23 -0600

    Added toggle for jsearch instruction popup

---
+++ public/js/login.js
@@ -0,0 +1,14 @@
+// Global Elements
+const popup = document.querySelector('#jsearch-key-popup');
+const popupBtn = document.querySelector('#jsearch-key-popup-button')
+const jsearchInstruction = document.querySelector('#jsearch-instructions')
+console.log(popupBtn)
+
+// Toggles view of jsearch instructions popup
+const jsearchPopupToggle = ()=> {
+    popup.classList.toggle('hidden')   
+}
+
+// Event listeners
+popupBtn.addEventListener('click', jsearchPopupToggle)
+jsearchInstruction.addEventListener('click', jsearchPopupToggle)
