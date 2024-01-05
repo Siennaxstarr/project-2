5e20720ea522d13957a4faefaa3956b876e27857
Kal-a11y <75392247+Kal-a11y@users.noreply.github.com>
2024-01-04 22:20:03 -0600

    Update index.js
    Fixed route reference

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const homeroutes = require('../homeroutes');
+const homeroutes = require('./homeroutes');
 router.use("/", homeroutes);
 
 module.exports = router;
