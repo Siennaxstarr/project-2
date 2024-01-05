0aa6d85e8b27050798e33e6fa060546e37709d36
Siennaxstarr <siennastarrv@gmail.com>
2024-01-04 20:11:42 -0800

    removed middleware placed with port server

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,4 @@
 const express = require('express');
 const app = express();
-const homeroutes = require('./routes/homeroutes');
 
-//middleware
-app.use('/', homeroutes);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+module.exports = router;
