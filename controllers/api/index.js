de4a8317902d2b02c88c6377a7b074c078fb62ed
Siennaxstarr <siennastarrv@gmail.com>
2024-01-04 19:33:31 -0800

    imported the routes from homeroutes and use middleware for the root path

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,12 @@
-const router = require('express').Router();
+const express = require('express');
+const app = express();
+const homeroutes = require('./routes/homeroutes');
 
+//middleware
+app.use('/', homeroutes);
 
-module.exports = router;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
