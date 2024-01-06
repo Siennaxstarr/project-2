c3681040798b0cd2a697d08d9ae9ae6acb840944
Kaila <kailaamia@gmail.com>
2024-01-05 22:58:40 -0600

    Removed test log and fixed formating

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,15 @@
 // Global Elements
 const popup = document.querySelector('#jsearch-key-popup');
-const popupBtn = document.querySelector('#jsearch-key-popup-button')
-const jsearchInstruction = document.querySelector('#jsearch-instructions')
-console.log(popupBtn)
+const popupBtn = document.querySelector('#jsearch-key-popup-button');
+const jsearchInstruction = document.querySelector('#jsearch-instructions');
+
+
 
 // Toggles view of jsearch instructions popup
 const jsearchPopupToggle = ()=> {
-    popup.classList.toggle('hidden')   
+    popup.classList.toggle('hidden');   
 }
 
 // Event listeners
-popupBtn.addEventListener('click', jsearchPopupToggle)
-jsearchInstruction.addEventListener('click', jsearchPopupToggle)
+popupBtn.addEventListener('click', jsearchPopupToggle);
+jsearchInstruction.addEventListener('click', jsearchPopupToggle);
