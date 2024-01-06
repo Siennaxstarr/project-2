84c50ef0d5fd682595e7aee61e798301edfb7e3d
Kaila <kailaamia@gmail.com>
2024-01-06 15:27:49 -0600

    Added event listener that login in user

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,9 @@ const popup = document.querySelector('#jsearch-key-popup');
 const popupBtn = document.querySelector('#jsearch-key-popup-button');
 const jsearchInstruction = document.querySelector('#jsearch-instructions');
 
-
+const loginUsername = document.querySelector('#login-username');
+const loginPassword = document.querySelector('#login-password');
+const loginBtn = document.querySelector('#login-button');
 
 // Toggles view of jsearch instructions popup
 const jsearchPopupToggle = ()=> {
@@ -13,3 +15,25 @@ const jsearchPopupToggle = ()=> {
 // Event listeners
 popupBtn.addEventListener('click', jsearchPopupToggle);
 jsearchInstruction.addEventListener('click', jsearchPopupToggle);
+
+//Logs in user
+loginBtn.addEventListener('click', async (event)=> {
+    event.preventDefault();
+
+    const username = loginUsername.value.trim();
+    const password = loginPassword.value.trim();
+    console.log(username)
+
+    const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/job-search');
+      } else {
+        alert('Failed to log in');
+
+      }
+})
