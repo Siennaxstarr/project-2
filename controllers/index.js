c6b237b6892c71a38d35d877b569f6528ca85f20
Kaila <kailaamia@gmail.com>
2024-01-06 15:25:49 -0600

    Added routes to the indexes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
 const homeroutes = require('./homeroutes');
+const apiRoutes = require('./api');
+
 router.use("/", homeroutes);
+router.use('/api', apiRoutes)
 
 module.exports = router;
