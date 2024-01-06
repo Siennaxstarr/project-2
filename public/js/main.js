b2bc0e9cdb662ff898ed849094006fa4fb23b781
Kaila <kailaamia@gmail.com>
2024-01-06 16:35:02 -0600

    Added style change when path is active

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,12 @@
 //Global Elements
 const hamburger = document.querySelector('#hamburgerIcon svg');
 const exitHamburger = document.querySelector('#exitHamburgerIcon svg');
-const mobileMenuButton = document.querySelector('#mobile-menu-button')
-const mobileMenu = document.querySelector('#mobile-menu')
+const mobileMenuButton = document.querySelector('#mobile-menu-button');
+const mobileMenu = document.querySelector('#mobile-menu');
+
+const homeTabs = document.querySelectorAll('a[href="/"]');
+const jobSearchTabs = document.querySelectorAll('a[href="/job-search"]');
+const applicationTabs = document.querySelectorAll('a[href="/applications"]');
 
 //Toggles mobile menu navigation
 mobileMenuButton.addEventListener('click', () => {
@@ -14,4 +18,17 @@ mobileMenuButton.addEventListener('click', () => {
 
 	mobileMenu.classList.toggle('hidden');
 
-})
+});
+
+//Changes active tab in navigation
+switch (window.location.pathname) {
+	case '/':
+		Array.from(homeTabs).map(tab => tab.classList.add('bg-gray-900', 'text-white'));
+		break;
+	case '/job-search':
+		Array.from(jobSearchTabs).map(tab => tab.classList.add('bg-gray-900', 'text-white'));		
+		break;
+	case '/applications':
+		Array.from(applicationTabs).map(tab => tab.classList.add('bg-gray-900', 'text-white'));		
+		break;
+}
