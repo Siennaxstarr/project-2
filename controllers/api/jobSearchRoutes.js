a9a64e6f571ab4a6b400b1d64445c1e48b8e4363
Kaila <kailaamia@gmail.com>
2024-01-08 20:54:14 -0600

    Added api route

--- controllers/api/jobSearchRoutes.js
+++ controllers/api/jobSearchRoutes.js
@@ -3,32 +3,32 @@ const History = require('../../models/searchHistory');
 
 router.post('/', async (req, res) => {
     try {
-       const searchTerm = encodeURIComponent(req.body.searchTerm);
-       const url = `https://jsearch.p.rapidapi.com/search?query=${searchTerm}&num_pages=1`;
-       const options = {
-           method: 'GET',
-           headers: {
-               'X-RapidAPI-Key': '0b9700a505msh43a8ab52968bbeep18341fjsn0dc3a212ea99',
-               'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-           }
-       };
-   
-   
-       const response = await fetch(url, options);
-       const searchResults = await response.json();
-       const jobResults = searchResults.data;
-       const newHistory = await History.create({
-        term: req.body.searchTerm,
-        result: jobResults
-       });
+        const searchTerm = encodeURIComponent(req.body.searchTerm);
+        const url = `https://jsearch.p.rapidapi.com/search?query=${searchTerm}&num_pages=1`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '0b9700a505msh43a8ab52968bbeep18341fjsn0dc3a212ea99',
+                'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+            }
+        };
 
 
-       res.status(201).json(newHistory);
-   } catch (err) {
-    console.log(err)    
-    res.status(500).json(err)
-   }
-    
+        const response = await fetch(url, options);
+        const searchResults = await response.json();
+        const newHistory = await History.create({
+            term: req.body.searchTerm,
+            result: searchResults.data
+        });
+        console.log(searchResults.data)
+
+
+        res.status(201).json(newHistory);
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+
 })
 
 module.exports = router;
