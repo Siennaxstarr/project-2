2cdf51a34510bd509cd258280dce7067e19097ec
Kaila <kailaamia@gmail.com>
2024-01-10 14:47:14 -0600

    Added jsearch session key and clear form for api key

--- controllers/api/jobsearchroutes.js
+++ controllers/api/jobsearchroutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const History = require('../../models/searchHistory');
+require('dotenv').config();
+
 
 router.post('/', async (req, res) => {
     try {
@@ -8,7 +10,7 @@ router.post('/', async (req, res) => {
         const options = {
             method: 'GET',
             headers: {
-                'X-RapidAPI-Key': '0b9700a505msh43a8ab52968bbeep18341fjsn0dc3a212ea99',
+                'X-RapidAPI-Key': process.env.API_KEY,
                 'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
             }
         };
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -30,6 +30,7 @@ router.post('/login', async (req, res) => {
         req.session.save(() => {
           req.session.user_id = userData.id;
           req.session.logged_in = true;
+          req.session.key = userData.jsearchKey;
           
           res.json({ user: userData, message: 'You are now logged in!' });
         });
